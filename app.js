18464d72f418fb9ca0d9fdbd1e4b9048218826a4
Robin <daorob@gmail.com>
2020-04-02 15:45:17 -0500

    Make a marker appear for Tokyo Tower.

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ let map;
 function initMap() {
     // TODO: use a constructor to create a new map JS object. You can use the coordinates
     map = new google.maps.Map(document.getElementById('map'), {
+        // coordinates for tokyo tower: 35.6586° N, 139.7454° E
         center: { lat: 35.6586, lng: 139.7454 },
         zoom: 13
     });
-    // coordinates for tokyo tower: 35.6586° N, 139.7454° E
+
+    // this is a marker instance
+    let tokyoTower = { lat: 35.6586, lng: 139.7454 };
+    let market = new google.maps.Marker({
+        position: tokyoTower,
+        map: map,
+        title: 'Tokyo Tower!'
+    });
 }
